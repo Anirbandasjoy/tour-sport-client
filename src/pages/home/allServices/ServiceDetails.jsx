0b9bf3971f3d65fce31da97234e475ad8abd4461
joydas <joy600508@gmail.com>
2023-11-08 15:02:15 +0600

    adding farmer motion scroll animation

--- src/pages/home/allServices/ServiceDetails.jsx
+++ src/pages/home/allServices/ServiceDetails.jsx
@@ -167,13 +167,13 @@ const ServiceDetails = () => {
                                 const { serviceImage, serviceName, servicePrice, serviceProviderImage, serviceProviderName, _id } = service || {}
                                 return <div key={_id} className="details">
                                     <div className="flex flex-col sm:flex-row lg:flex-col  cursor-pointer  bg-white border border-gray-200 rounded-lg shadow   hover:bg-gray-100 relative dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                                        <img className="object-cover w-full lg:w-full sm:w-60 rounded-t-lg  h-full md:h-auto  md:rounded-none md:rounded-l-lg" src={serviceImage} />
+                                        <img className="object-cover w-full lg:w-full sm:w-60 rounded-t-lg  h-24 md:h-auto  md:rounded-none md:rounded-l-lg" src={serviceImage} />
                                         {/* <div className=" overlay w-full lg:w-full rounded-bl-md hidden sm:block md:w-60 rounded-t-lg h-96  object-cover sm:h-[14.3rem] lg:h-[12rem] absolute ">
                                         </div> */}
                                         <div className="flex flex-col justify-between p-4 leading-normal">
                                             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{serviceName}</h5>
                                             <h1 className="lg:text-xl text-[#ff976a] font-bold text-sm">${servicePrice} <span className="text-gray-500"> / </span> <span className="lg:text-sm text-xs text-gray-500 ">Per Person</span> </h1>
-                                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{serviceDsc.slice(0, 80)}</p>
                                             <div className="flex items-center mb-3 ">
                                                 <div className="flex items-center gap-2">
                                                     <img className="w-6 rounded-full" src={serviceProviderImage} alt={serviceProviderName} />
